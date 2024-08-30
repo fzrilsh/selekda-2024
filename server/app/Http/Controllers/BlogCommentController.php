3677adb6fdb7c466b59707c7b9211bec5a94963d
fzrilsh <fazriloke18@gmail.com>
2024-08-30 19:58:38 +0700

    Fix captcha bug

--- server/app/Http/Controllers/BlogCommentController.php
+++ server/app/Http/Controllers/BlogCommentController.php
@@ -38,6 +38,7 @@ class BlogCommentController extends Controller implements HasMiddleware
             'comment' => 'required|string',
             'captcha' => ['required', function(string $attribute, mixed $value, Closure $fail){
                 $id = explode('|', $value)[0];
+                dd($id);
                 $captcha = Captcha::query()->find($id);
                 if(!$captcha || !CaptchaController::check($captcha, $value)) $fail('Captcha is invalid.');
             }]
